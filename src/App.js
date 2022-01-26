0fdfd14473df8badadc5c816645cc0909a1106e3
Sudhanshu Kumar <64831792+Sidhudog@users.noreply.github.com>
2022-01-26 21:15:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import axios from 'axios';
+import './App.css'
 
-function App() {
+const App = () => {
+
+    const [advice,setAdvice] = useState('');
+    const [bgimage, setBgimage] = useState('');
+    const url = "https://api.adviceslip.com/advice";
+    const ClientId ="wNLZLEboBp9ux77jTpCT_kCI21Jt4z3nPleek55sbHE";
+    const img_url = `https://api.unsplash.com/photos/random/?client_id=${ClientId}`;
+    const fetchAdvice=() =>{
+        axios
+          .get(url)
+          .then((response) => {
+            setAdvice(response.data.slip.advice);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        axios
+            .get(img_url)
+            .then((response) => {
+                console.log(
+                  response.data.urls.raw + "&query=scenary&h=900&w=1200"
+                );
+                setBgimage(response.data.urls.raw + '&query=scenary&h=900&w=1200');
+            // setBgimage(response);
+            })
+            .catch((error) => {
+            console.log(error);
+            });
+    }
+    
+
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{backgroundImage:`url(${bgimage})`,}} className="App">
+        <div className="Card">
+          <h1>{advice}</h1>
+        </div>
+        <button className="btn" onClick={fetchAdvice}>REFRESH</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
